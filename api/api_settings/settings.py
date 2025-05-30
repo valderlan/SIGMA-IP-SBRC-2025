d674ad066d8ea5efff65f8556d9dfc53ef9aef3d
Davi de Oliveira <olidavi031130@gmail.com>
2025-05-30 17:47:39 -0300

    fix: resolvido problema no caminho do .env & adição no requirements

--- api/api_settings/settings.py
+++ api/api_settings/settings.py
@@ -16,7 +16,9 @@ import json
 from dotenv import load_dotenv
 import json
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), "..", "netcontrol", ".env")
+load_dotenv(dotenv_path)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
