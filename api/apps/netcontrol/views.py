ce9b1f541f9c5378fbcab4f4e2f1f94cb65748bd
Davi de Oliveira <olidavi031130@gmail.com>
2025-07-16 15:25:35 -0300

    chore: documentação no swagger com extend_schema para as views

--- api/apps/netcontrol/views.py
+++ api/apps/netcontrol/views.py
@@ -10,12 +10,38 @@ from apps.netcontrol.pagination import GenericPagination
 from apps.netcontrol.serializers import (BlacklistSerializer, WhitelistSerializer, TarpitSerializer, SuspectSerializer)
 from apps.netcontrol.filters import (BlacklistFilter, WhitelistFilter, TarpitFilter, SuspectFilter)
 from apps.netcontrol.services import filtrar_tarpit
+from drf_spectacular.utils import extend_schema_view, extend_schema
 import time
 import logging
 
 logger = logging.getLogger(__name__)
 
-# view para blacklist
+@extend_schema_view(
+    create=extend_schema(
+        summary="Creates a blacklist object",
+        description="Creates a new blacklist object.",
+    ),
+    list=extend_schema(
+        summary="List all blacklist objects",
+        description="Returns a list of all blacklist objects.",
+    ),
+    retrieve=extend_schema(
+        summary="Retrieve a specific blacklist object",
+        description="Returns a blacklist object by its IP.",
+    ),
+    update=extend_schema(
+        summary="Update a blacklist object",
+        description="Updates an blacklist object by its IP.",
+    ),
+    partial_update=extend_schema(
+        summary="Partially update an blacklist object",
+        description="Partially updates a blacklist object by its IP.",
+    ),
+    destroy=extend_schema(
+        summary="Delete a blacklist object",
+        description="Deletes a blacklist object by its IP",
+    ),
+)
 class BlacklistViewSet(viewsets.ModelViewSet):
     queryset = Blacklist.objects.all().order_by('id')
     serializer_class = BlacklistSerializer
@@ -28,7 +54,32 @@ class BlacklistViewSet(viewsets.ModelViewSet):
     lookup_field = "ip_address"
 
 
-# view para whitelist
+@extend_schema_view(
+    create=extend_schema(
+        summary="Creates a whitelist object",
+        description="Creates a new whitelist object.",
+    ),
+    list=extend_schema(
+        summary="List all whitelist objects",
+        description="Returns a list of all whitelist objects.",
+    ),
+    retrieve=extend_schema(
+        summary="Retrieve a specific whitelist object",
+        description="Returns a whitelist object by its IP.",
+    ),
+    update=extend_schema(
+        summary="Update a whitelist object",
+        description="Updates an whitelist object by its IP.",
+    ),
+    partial_update=extend_schema(
+        summary="Partially update an whitelist object",
+        description="Partially updates a whitelist object by its IP.",
+    ),
+    destroy=extend_schema(
+        summary="Delete a whitelist object",
+        description="Deletes a whitelist object by its IP",
+    ),
+)
 class WhitelistViewSet(viewsets.ModelViewSet):
     queryset = Whitelist.objects.all().order_by('id')
     serializer_class = WhitelistSerializer
@@ -41,7 +92,32 @@ class WhitelistViewSet(viewsets.ModelViewSet):
     lookup_field = "ip_address"
 
 
-# view para tarpit
+@extend_schema_view(
+    create=extend_schema(
+        summary="Creates a tarpit object",
+        description="Creates a new tarpit object.",
+    ),
+    list=extend_schema(
+        summary="List all tarpit objects",
+        description="Returns a list of all tarpit objects.",
+    ),
+    retrieve=extend_schema(
+        summary="Retrieve a specific tarpit object",
+        description="Returns a tarpit object by its IP.",
+    ),
+    update=extend_schema(
+        summary="Update a tarpit object",
+        description="Updates an tarpit object by its IP.",
+    ),
+    partial_update=extend_schema(
+        summary="Partially update an tarpit object",
+        description="Partially updates a tarpit object by its IP.",
+    ),
+    destroy=extend_schema(
+        summary="Delete a tarpit object",
+        description="Deletes a tarpit object by its IP",
+    ),
+)
 class TarpitViewSet(viewsets.ModelViewSet):
     queryset = Tarpit.objects.all().order_by('id')
     serializer_class = TarpitSerializer
@@ -80,6 +156,32 @@ class TarpitViewSet(viewsets.ModelViewSet):
         return Response(data, status=201)
     
 
+@extend_schema_view(
+    create=extend_schema(
+        summary="Creates a suspect object",
+        description="Creates a new suspect object.",
+    ),
+    list=extend_schema(
+        summary="List all suspect objects",
+        description="Returns a list of all suspect objects.",
+    ),
+    retrieve=extend_schema(
+        summary="Retrieve a specific suspect object",
+        description="Returns a suspect object by its IP.",
+    ),
+    update=extend_schema(
+        summary="Update a suspect object",
+        description="Updates an suspect object by its IP.",
+    ),
+    partial_update=extend_schema(
+        summary="Partially update an suspect object",
+        description="Partially updates a suspect object by its IP.",
+    ),
+    destroy=extend_schema(
+        summary="Delete a suspect object",
+        description="Deletes a suuspect object by its IP",
+    ),
+)
 class SuspectViewSet(viewsets.ModelViewSet):
     queryset = Suspect.objects.all().order_by('id')
     serializer_class = SuspectSerializer
