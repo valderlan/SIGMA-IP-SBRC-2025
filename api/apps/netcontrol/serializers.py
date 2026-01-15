842421d057a54ff2d9901c8a53fc34a1631dc4dd
Davi de Oliveira <olidavi031130@gmail.com>
2026-01-15 17:27:32 -0300

    refactor: import only required models

--- api/apps/netcontrol/serializers.py
+++ api/apps/netcontrol/serializers.py
@@ -1,25 +1,25 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from apps.netcontrol.models import Blacklist, Analysis, Whitelist, Suspect
 
-class BlacklistSerializer(serializers.ModelSerializer):
+class BlacklistSerializer(ModelSerializer):
     class Meta:
         model = Blacklist
         fields = '__all__'
 
 
-class WhitelistSerializer(serializers.ModelSerializer):
+class WhitelistSerializer(ModelSerializer):
     class Meta:
         model = Whitelist
         fields = '__all__'
 
 
-class AnalysisSerializer(serializers.ModelSerializer):
+class AnalysisSerializer(ModelSerializer):
     class Meta:
         model = Analysis
         fields = '__all__'
         
 
-class SuspectSerializer(serializers.ModelSerializer):
+class SuspectSerializer(ModelSerializer):
     class Meta:
         model = Suspect
         fields = '__all__'
